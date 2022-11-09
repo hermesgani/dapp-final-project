19225b40223d6960ffa3c022738c8fea5d32c7fc
Hermes <hermes.v.gani@gdplabs.id>
2022-11-09 10:31:58 +0700

    Install dependencies and add api.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    domains: [
+      'ipfs.infura.io',
+      'statics-polygon-lens-staging.s3.eu-west-1.amazonaws.com',
+      'lens.infura-ipfs.io',
+      'source.unsplash.com',
+      ""
+    ],
+  },
 };
