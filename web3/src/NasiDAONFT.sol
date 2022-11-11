ab3915b651af707ae87c2704eeef9e1d648cddf0
Zulchaidir <zulchaidir@gdplabs.id>
2022-11-11 15:10:59 +0700

    add ownerdata

--- web3/src/NasiDAONFT.sol
+++ web3/src/NasiDAONFT.sol
@@ -9,10 +9,25 @@ contract NasiDAONFT is ERC721("Nasi DAO NFT", "NADA"), Ownable {
     using Counters for Counters.Counter;
     Counters.Counter internal _tokenIds;
 
+    struct OwnerData {
+        address ownerAddress;
+        string profileId;
+        uint256 tokenId;
+    }
+
     string constant ipfsURI =
         "ipfs://QmQubJS4KtK49i458kGe4BosUP6WNjBrJ8zqVoTEYSa3jg";
 
     mapping(address => uint256) public nftOwner;
+    OwnerData[] public owners;
+
+    function registerOwner(address to, string memory _profileId)
+        public
+        onlyOwner
+    {
+        uint tokenId = mint(to);
+        owners.push(OwnerData(to, _profileId, tokenId));
+    }
 
     function mint(address to) public onlyOwner returns (uint) {
         require(nftOwner[to] == 0, "You already had an NFT");
@@ -25,5 +40,9 @@ contract NasiDAONFT is ERC721("Nasi DAO NFT", "NADA"), Ownable {
         _tokenIds.increment();
 
         return newId;
+    }
+
+    function getAllOwners() public view onlyOwner returns (OwnerData[] memory) {
+        return owners;
     }
 }
